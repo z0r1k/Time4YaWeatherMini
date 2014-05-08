fd8e873e522d13dab7a35d0e7f650ed9a3e066d6
Michael Leibenson <gramatron@gmail.com>
2014-05-08 17:54:01 +0200

    adding media query
    Adding media query to make it look a bit better. Still horrible and
    super ugly, but…

--- time4yaweathermini.js
+++ time4yaweathermini.js
@@ -1,23 +1,50 @@
-﻿// ==UserScript==
+// ==UserScript==
 // @name        Time4YaWeather
 // @grant       none
 // @include     http://mini.pogoda.yandex.ru/frankfurt%20am%20main
-// @version     0.2
+// @version     0.3
 // ==/UserScript==
 
 (function(){
-    var 
+
+    var
         // css and html templates
-        html = '<p class="time"></p><p class="date"></p>',
-        css  = 'body{overflow: hidden;} .date-time{margin: 100px auto 0; text-align: center;} .time{font-size: 10em;} .date{color: #999999; font-weight: normal;}',
+        html     = '<p class="time"></p><p class="date"></p>',
+        css      = '\
+            body {\
+                overflow: hidden;\
+            }\
+            \
+            .date-time {\
+                margin: 60px auto 0;\
+                text-align: center;\
+            }\
+            \
+            .time {\
+                font-size: 6em;\
+            }\
+            \
+            .date {\
+                color: #999999;\
+                font-weight: normal;\
+            }\
+            @media (min-height: 900px) {\
+                .date-time {\
+                    margin-top: 100px;\
+                }\
+                .time {\
+                    font-size: 10em;\
+                }\
+            }\
+        ',
         // how often time node will be updated
         INTERVAL = 1000;
 
-    var 
+    var
         // static data
         days = [
             "Понедельник",
-            "Вторник",        
+            "Вторник",
             "Среда",
             "Четверг",
             "Пятница"
@@ -37,38 +64,41 @@
             "Декабрь"
         ];
 
-    var 
+    var
         // get formated time
         _getTime = function() {
-            var 
-                date = new Date(),        
-                hours   = 0, 
+            var
+                date    = new Date(),
+                hours   = 0,
                 minutes = 0,
                 seconds = 0;
-                
-            var _addZero = function(num) {
-                num = parseInt(num, 10);
-                if (num < 10) {
-                    num = "0" + num;
-                }
-                return num;
-            };
+
+            var
+                _addZero = function(num) {
+                    num = parseInt(num, 10);
+                    if (num < 10) {
+                        num = "0" + num;
+                    }
+                    return num;
+                };
 
             hours   = _addZero(date.getHours());
             minutes = _addZero(date.getMinutes());
             seconds = _addZero(date.getSeconds());
-            
+
             return hours + " " + minutes + " " + seconds;
         },
         // get formated date
         _getDate = function() {
-            var date = new Date();
-            return days[date.getUTCDay() - 1] + ", " + 
+            var
+                date = new Date();
+
+            return days[date.getUTCDay() - 1] + ", " +
                    months[date.getUTCMonth()] + " "  +
-                   date.getUTCDate();    
+                   date.getUTCDate();
         };
 
-    var 
+    var
         // head-tag
         headNode    = document.querySelector("head"),
         // node with weather data
@@ -96,13 +126,14 @@
         timeNode = document.querySelector(".time");
         dateNode = document.querySelector(".date");
 
-        // populate time and date nodes with content       
+        // populate time and date nodes with content
         timeNode.innerHTML = _getTime();
         dateNode.innerHTML = _getDate();
-           
+
         // update time every second
         setInterval(function(){
             timeNode.innerHTML = _getTime();
         }, INTERVAL);
     }
+
 })();
